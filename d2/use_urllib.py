c3e2a6c02421de19d1dda4e69614b2f4fe7852d0
yangpuqing <2413759410@qq.com>
2016-07-30 00:14:39 +0800

    it is diffucult

---
+++ d2/use_urllib.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#urllib的request模块可以非常方便地抓取URL内容，也就是发送一个GET请求到指定的页面，然后返回HTTP的响应：
+from urllib import request
+
+with request.urlopen('https://api.douban.com/v2/book/2129650') as f:
+    data = f.read()
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    print('Data:', data.decode('utf-8'))
